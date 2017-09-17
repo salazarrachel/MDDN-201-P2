e80498f1961b8f52ef2e67449d7c3576c064b2ad
Rachel Salazar <salazarach@d-03-06-lab.mddn.vuw.ac.nz>
2017-09-18 08:39:25 +1200

    last upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function clickpage1() {
     document.getElementById("page1").style.display = "block";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "none";
     document.getElementById("loader").style.display = "none";
 }
 
@@ -9,19 +10,48 @@ function clickpage2() {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "block";
     document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "none";
 
 }
+
+
+
 
 function clickpage3() {
     document.getElementById("page1").style.display = "none";
     document.getElementById("page2").style.display = "none";
     document.getElementById("page3").style.display = "block";
+    document.getElementById("page4").style.display = "none";
 }
+
+function clickpage4() {
+    document.getElementById("page1").style.display = "none";
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("page3").style.display = "none";
+    document.getElementById("page4").style.display = "block";
+}
+
+
+
 $(document).ready(function(){
 	$("#page1").click(function(){
     $("#loader").fadeOut(5000);
     $("#page3").fadeIn(15000);
+    });
+
+    $("#page3").click(function(){
+        $("#page3").fadeOut(5000);
+        $("#page4").fadeIn(12000);
+
+    
+
+
+    });
+
+
+
 
 });
 
-});
+
+
